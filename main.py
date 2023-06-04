9c4737499b78b3d72700982cc597097d45014269
bengabp <bengabdev4@gmail.com>
2023-06-04 20:39:43 +0100

    Commited some old unknown changes

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-from playwright.sync_api import sync_playwright,BrowserContext
+from playwright.sync_api import sync_playwright, BrowserContext, TimeoutError
 from pprint import pprint
 import json
-from config import DIR_PATH,logger
+from config import DIR_PATH, logger
 
 
 class NetnaijaCrawler:
@@ -13,7 +13,7 @@ class NetnaijaCrawler:
 	def run(self):
 		self.logger.info("Starting crawler ...")
 		with sync_playwright() as playwright_sync:
-			browser = playwright_sync.chromium.launch(headless = True)
+			browser = playwright_sync.chromium.launch(headless = False)
 			context = browser.new_context()
 			page = context.new_page()
 			
@@ -21,6 +21,12 @@ class NetnaijaCrawler:
 			movie_elements = page.query_selector_all("div.video-files > article.file-one.shadow")
 			
 			movies = []
+			
+			with open("movies.json") as movies_json:
+				movies = json.load(movies_json)
+			
+			for movie in movies:
+				self.get_movie_details(movie, context)
 			"""
 			for movie_element in movie_elements:
 				image_url = movie_element.query_selector("div.thumbnail img").get_property("src").__str__()
@@ -48,12 +54,24 @@ class NetnaijaCrawler:
 				json.dump(movies,movies_json,indent=4)
 			# Scrape movie description
 		"""
-
-	def get_movie_details(self,details_url,context:BrowserContext):
+	
+	def get_movie_details(self, movie, context: BrowserContext):
+		details_url = movie['details_url']
+		
 		details_page = context.new_page()
 		details_page.goto(details_url)
 		print(details_page.title())
-	
+		
+		retry_click = True
+		while retry_click:
+			ads_click_interceptor = details_page.locator("div.lmslmx")
+			details_page.mouse.click(100, 100)
+			try:
+				details_page.get_by_role("link", name = " Download  (Video)").click(timeout = 10, click_count = 1)
+				self.logger.info("Clicked")
+				break
+			except TimeoutError:
+				self.logger.info("Retry click ...")
 
 
 if __name__ == "__main__":
