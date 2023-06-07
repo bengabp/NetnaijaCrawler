f9486e7dde3fd1f65a76a92933cde12119d77503
RosarioDiBartolo <117893062+RosarioDiBartolo@users.noreply.github.com>
2023-06-07 23:04:54 +0200

    Update main.py
    Deleting pointless code, moving some in Test.py

--- main.py
+++ main.py
@@ -2,13 +2,10 @@ import requests
 from config import logger
 from bs4 import BeautifulSoup
 
-base_movies_url = "https://www.thenetnaija.net/videos/movies"
-
+ 
 class NetnaijaCrawler:
 	def __init__(self):
 		self.logger = logger
-		self.base_url = "https://www.thenetnaija.net"
-		self.movies_url = f"{self.base_url}/videos/movies"
 
 
 	def getMoviesList(self, page = 0):
@@ -30,7 +27,7 @@ class NetnaijaCrawler:
 			'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36 Edg/114.0.1823.37',
 		}
 
-		r = requests.get(f"{base_movies_url}/page/{page}", headers = headers )
+		r = requests.get(f"https://www.thenetnaija.net/videos/movies/page/{page}", headers = headers )
 
 		doc = BeautifulSoup(r.text, "html.parser")
 
@@ -84,13 +81,4 @@ class NetnaijaCrawler:
 		response = requests.get(f'https://api.sabishare.com/token/download/{SabiShareMovieId}', headers=headers)
 
 		return response.json()["data"]["url"]
-Crawler = NetnaijaCrawler()
-
-Movies = Crawler.getMoviesList(5)
-
-
-name = Crawler.getFilmName(Movies[0])
-
-link = Crawler.getDownloadLink(name)
-
-print(link)
+ 
